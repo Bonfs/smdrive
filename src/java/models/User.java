05de13c4deeca7cc2371f81fdb077c573f9736fd
bonfs <bonfim@alu.ufc.br>
2017-11-21 16:46:37 -0300

    bd funcionando com postgres

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -9,12 +9,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author bonfim
  */
 @Entity
+@Table(name="\"User\"")
 public class User {
     
     @Id @GeneratedValue
